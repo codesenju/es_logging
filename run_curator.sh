7c24774d7587f7b015bbb40015ad6ff64b3373c7
Lehlogonolo Masubelele <lehlogonolo.masubelele@fnb.co.za>
2021-06-23 10:17:51 +0200

    script modification

--- run_curator.sh
+++ run_curator.sh
@@ -1,1 +1,5 @@
-docker run --rm --name curator -v /opt/devops/docker_run/es_logging/curator:/tmp --net monitor --entrypoint curator <registry>/elasticsearch-curator:5.8.4 /tmp/curator-action.yml --config /tmp/curator-conf.yml --dry-run
+#!/bin/bash
+echo "====================================================="
+echo "Curator started at - $(date +'%d-%m-%Y*%T')"
+echo "====================================================="
+docker run --rm --name curator -v /opt/devops/docker_run/es_logging/curator:/tmp --net monitor --entrypoint curator <registry>/elasticsearch-curator:5.8.4 /tmp/curator-action.yml --config /tmp/curator-conf.yml
